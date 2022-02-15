9700eae8ecd89eb46dd334f6e7a87e832d57e977
VECTOR-SOFTWARE\vdubravskyi <vdubravskyi@vectore-software.com>
2022-02-15 19:30:40 +0200

    added solution to the task

---
+++ Task/Task/Classes/Solution.cs
@@ -0,0 +1,80 @@
+﻿namespace Task.Classes
+{
+    public class Solution
+    {
+        public string GetSolution(string s, string t)
+        {
+            if (s.Length - t.Length < -1 || s.Length > t.Length)
+            {
+                return "IMPOSSIBLE";
+            }
+
+            if (s.Equals(t))
+            {
+                return "EQUAL";
+            }
+
+            if (s.Length - t.Length == -1)
+            {
+                var isStringsHasNotRelevantChars = false;
+
+                var insertingValue = ' ';
+
+                for (var i = 0; i < s.Length; i++)
+                {
+                    if (s[i] != t[i])
+                    {
+                        if (isStringsHasNotRelevantChars && s.Substring(i) != t.Substring(i + 1))
+                        {
+                            return "IMPOSSIBLE";
+                        }
+
+                        if (!isStringsHasNotRelevantChars)
+                        {
+                            insertingValue = t[i];
+
+                            isStringsHasNotRelevantChars = true;
+                        }
+                    }
+                }
+
+                return $"INSERT {insertingValue}";
+            }
+            else
+            {
+                var isStringsHasNotRelevantChars = false;
+
+                var replaceWith = ' ';
+                var shouldBeReplaced = ' ';
+
+                var firstNotRelevantChar = ' ';
+
+                for (var i = 0; i < t.Length; i++)
+                {
+                    if (s[i] != t[i])
+                    {
+                        if (isStringsHasNotRelevantChars)
+                        {
+                            if (firstNotRelevantChar == s[i] &&
+                                (s.Substring(i + 1) == t.Substring(i + 1) || t.Length == i - 1))
+                            {
+                                return $"SWAP {t[i]} {firstNotRelevantChar}";
+                            }
+
+                            return "IMPOSSIBLE";
+                        }
+
+                        firstNotRelevantChar = t[i];
+
+                        shouldBeReplaced = s[i];
+                        replaceWith = t[i];
+
+                        isStringsHasNotRelevantChars = true;
+                    }
+                }
+
+                return $"REPLACE {shouldBeReplaced} {replaceWith}";
+            }
+        }
+    }
+}
---
+++ Task/Task/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Task.Classes;
+
+namespace Task
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var solution = new Solution();
+
+
+            Console.WriteLine(solution.GetSolution("nice", "niece"));
+            Console.WriteLine(solution.GetSolution("nece", "niece"));
+            Console.WriteLine(solution.GetSolution("test", "tent"));
+            Console.WriteLine(solution.GetSolution("form", "from"));
+            Console.WriteLine(solution.GetSolution("abcdr", "abcrd"));
+            Console.WriteLine(solution.GetSolution("bacde", "abcde"));
+            Console.WriteLine(solution.GetSolution("o", "odd"));
+            Console.WriteLine(solution.GetSolution("odd", "odd"));
+
+        }
+    }
+}
